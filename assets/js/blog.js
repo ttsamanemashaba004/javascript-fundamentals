20f63bc421ec36fddcbc7a61cf0bc9f3ef4e2f56
ttsamanemashaba004 <ttsamane.mashaba@gmail.com>
2024-09-16 18:39:29 +0200

    Fined with the OOP blog.js need to finalize by creating repo for blog.js

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -17,22 +17,47 @@ class Blog{
      * @param {string} content - The content of the blog post
      */
 
+
+    
+
     createPost(title, content){
         const newPost = new Post(title, content, this.username);
         this.posts.push(newPost);
-        console.log(`Post titled "${title}" created by ${this.username}`)
+        
     }
 
-    editPost(){
-
+    editPost(title, newContent){
+        for(let i = 0; i < this.posts.length; i++){
+            let n = 0;
+            if(this.posts[i].title == title){
+                this.posts[i].content = newContent;
+                console.log(`Post that is titled "${title} has been updated"`);
+                return; // exit
+            }
+        }
+        console.log(`No post found with this title "${title}"`);
     }
 
-    deletePost(){
-
+    deletePost(title){
+        for(let i = 0; i < this.posts.length; i++){
+            if(this.posts[i].title === title){
+                this.posts.splice(i, 1);
+                console.log(`Post titled "${title}" has been deleted.`);
+                return; // exit
+            }
+        }
+        console.log(`No post found with this title "${title}"`);
     }
 
     displayPost(){
+        console.log(`Posts by ${this.username}:`);
+        this.posts.forEach((post) => {
+            post.showPost()
+        })
+    }
 
+    get getPosts(){
+        return this.posts;
     }
 }
 
@@ -41,16 +66,29 @@ class Blog{
  */
 
 class Post{
-    constructor(title, content, author){
+    constructor(title, content, username){
         this.title = title;
-        this.author = author;
-        this.author = author;
+        this.content = content;
+        this.username = username;
         this.createdAt = new Date();
+    }
+
+    /**
+     * Display the post content in a formatter manner
+     */
+
+    showPost(){
+        console.log(`@${this.username} posted on ${this.createdAt}:`)
+        console.log(this.content);
+        console.log(`--------------------`) // separator for clarity
     }
 }
 
 // Create users
-// Users will create posts
-// Display posts
-// Edit posts
-// Delete post
+const user1 = new Blog('johnDoe', 'John Doe')
+user1.displayPost();
+user1.createPost('Java', 'I live Java');
+user1.displayPost();
+// user1.deletePost('Java')
+
+console.log(user1.getPosts.length);
